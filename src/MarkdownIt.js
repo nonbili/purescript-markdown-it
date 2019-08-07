0a300db59469a645b2767b2d2196d862f08e6e1d
Ping Chen <remotenonsense@gmail.com>
2019-08-07 17:12:04 +0900

    Add newMarkdownIt and render

--- src/MarkdownIt.js
+++ src/MarkdownIt.js
@@ -4,3 +4,11 @@ exports.renderString_ = function(input) {
   var md = new MarkdownIt();
   return md.render(input);
 };
+
+exports.newMarkdownIt_ = function(opts) {
+  return new MarkdownIt(opts);
+};
+
+exports.render_ = function(input, md) {
+  return md.render(input);
+};
--- src/MarkdownIt.purs
+++ src/MarkdownIt.purs
@@ -1,13 +1,35 @@
 module MarkdownIt
   ( renderString
+  , MarkdownIt
+  , MarkdownItOptions
+  , newMarkdownIt
+  , render
   ) where
 
 import Prelude
 
+import Data.Options as O
 import Effect (Effect)
-import Effect.Uncurried (EffectFn1, runEffectFn1)
+import Effect.Uncurried (EffectFn1, EffectFn2, runEffectFn1, runEffectFn2)
+import Foreign (Foreign)
 
 -- | Parse and render markdown string to html string.
 renderString :: String -> Effect String
 renderString = runEffectFn1 renderString_
 foreign import renderString_ :: EffectFn1 String String
+
+data MarkdownIt
+
+data MarkdownItOptions
+
+-- | Create a new MarkdownIt instance
+newMarkdownIt :: O.Options MarkdownItOptions -> Effect MarkdownIt
+newMarkdownIt opts = runEffectFn1 newMarkdownIt_ $ O.options opts
+foreign import newMarkdownIt_ :: EffectFn1 Foreign MarkdownIt
+
+
+-- | Use an existing MarkdownIt instance to render markdown string to html
+-- | string.
+render :: String -> MarkdownIt -> Effect String
+render = runEffectFn2 render_
+foreign import render_ :: EffectFn2 String MarkdownIt String
